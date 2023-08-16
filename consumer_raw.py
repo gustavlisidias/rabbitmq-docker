16114892c4194df4fb0a98ae8b44935ebd9f9d4e
Gustavo Lisi Dias <gustavo.dias@interunion.com.br>
2023-08-16 15:28:59 -0300

    v1.0

---
+++ consumer_raw.py
@@ -0,0 +1,19 @@
+import pika
+
+def minhaCallback(ch, method, properties, body):
+    print(body)
+
+connection_parameters = pika.ConnectionParameters(
+    host="localhost",
+    port="5672",
+    credentials=pika.PlainCredentials(username="guest", password="guest")
+)
+
+channel = pika.BlockingConnection(connection_parameters).channel()
+
+channel.queue_declare(queue="data_queue", durable=True)
+
+channel.basic_consume(queue="data_queue", auto_ack=True, on_message_callback=minhaCallback)
+
+print('Listen RabbitMQ on port 5672')
+channel.start_consuming()
